ac2563dcb8291424380b826b4f4709c6311eb192
AlexSchmale <alexschmale2911@gmail.com>
2022-07-27 10:33:11 -0300

    subo ejercicio 2 completo

--- ejercicio2.js
+++ ejercicio2.js
@@ -2,4 +2,10 @@ let nombre = 'Alex';
 let edad = 22;
 let desarrollador = false;
 let fechaDeNacimiento = new Date(1999, 10, 29)
-console.log(nombre, edad, desarrollador, fechaDeNacimiento);
+let libroFavorito = {
+    titulo: "Harry Potter",
+    autor: "J.k.rowling",
+    fecha: new Date(1998, 5, 26),
+    url:"https://www.amazon.es/Harry-Potter-Piedra-Filosofal-Rowling/dp/8478884459"
+}
+console.log(nombre, edad, desarrollador, fechaDeNacimiento, libroFavorito);
