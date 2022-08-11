628cdecb6af65740b8a697267b46d0b4e5ab5acd
AlexSchmale <alexschmale2911@gmail.com>
2022-08-10 21:49:52 -0300

    subo el ejercico numero 5

---
+++ moduloCinco/ejercicio5.js
@@ -0,0 +1,17 @@
+
+//Crea un archivo JS que contenga las siguientes líneas
+//- Una variable que contenga tu altura en centímetros (entero)
+let alturaCentimetros = 185;
+//- Una variable que contenga tu altura en metros (número de coma flotante)
+let alturaMetros = 1.85;
+//- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let pesoKilogramos = 75.4;
+//- Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaRedondeada = Math.round(alturaMetros);
+console.log(alturaRedondeada);
+//- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoRedondeado = Math.floor(pesoKilogramos);
+console.log(pesoRedondeado);
+//- Una variable que contenga si "e
+let maximo = ((Number.MAX_VALUE + 1) == Number.MAX_VALUE)
+console.log(maximo);
